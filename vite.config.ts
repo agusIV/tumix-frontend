29f5937ee3dd448945438f18a0236ff9c58f152b
agustin veron <agusivanveron@hotmail.com>
2025-08-29 16:34:41 -0300

    commit inicial3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-app/' // Cambia por el nombre de tu repositorio
+  base: '/tumix-frontend/' // Cambia por el nombre de tu repositorio
 })
