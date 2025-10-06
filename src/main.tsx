1a580a95d7e5b890ee128fad62989568433309c7
agustin veron <agusivanveron@hotmail.com>
2025-10-06 16:59:33 -0300

    pantalla de carga

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -9,7 +9,7 @@ import { UsuarioProovedor } from "./contextAPI/usuarioContext.tsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-const inicializar = async () => {
+/*const inicializar = async () => {
   const lista = await cargarLista()
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
@@ -23,3 +23,34 @@ const inicializar = async () => {
 }
 
 inicializar()
+*/
+function Root() {
+  const [lista, setLista] = useState<any[] | null>(null)
+
+  useEffect(() => {
+    const fetchLista = async () => {
+      const data = await cargarLista()
+      setLista(data)
+    }
+    fetchLista()
+  }, [])
+  if (!lista) {
+    return (
+      <div>
+        <h2>Cargando...</h2>
+      </div>
+    )
+  }return(
+    <UsuarioProovedor>
+      <ListaProovedor listaInicial={lista}>
+        <App/>
+      </ListaProovedor>
+    </UsuarioProovedor>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Root />
+  </StrictMode>
+)
