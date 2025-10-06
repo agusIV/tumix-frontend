9835c2915cd3a9b477a9084a28c78e35d95f20c2
agustin veron <agusivanveron@hotmail.com>
2025-10-06 16:47:29 -0300

    ajustando rutas cambiando packjson

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig(({mode}) => ({
   plugins: [react()],
-  base: mode === "production" ? "/tumix-frontend/" : "/", // Cambia por el nombre de tu repositorio
+  base: mode === "production" ? "/tumix-frontend/" : "/",
 }))
